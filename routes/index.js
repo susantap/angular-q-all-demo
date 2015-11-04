65754b1a49216aa607a07302d619d15fa56f4990
susanta pattanayak <susanta.pattanayak@gmail.com>
2015-11-04 11:57:19 -0800

    Few more updates to fix issues

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,8 @@ router.post('/create', function (req, res, next) {
     var index = validator.checkForDuplicatePerson(data.name, data.age);
     if(index >= 0)
     {
-        error.push("Person with name: "+data.name+" and age: "+data.age+" already exist in the system.");
+        // as the person already exists return teh same
+        res.send(constructSuccess(person.persons[index]))
 
     }
     if(!validator.isInteger(data.age))
@@ -35,7 +36,7 @@ router.post('/create', function (req, res, next) {
         error.push("Person with name: "+data.name+" and age: "+data.age+" age should be a number, not a string.");
     }
 
-    if(error.length == 0)
+    if(error.length == 0 )
     {
         var per = person.addPerson(data);
         if (per) {
@@ -73,7 +74,7 @@ router.post('/addaddress',function(req, res,next){
     {
         error.push("City: "+reqObj.address.city+" is not a valid City.Please provide a valid String for City");
     }
-    if(validator.isInteger(reqObj.address.city))
+    if(validator.isInteger(reqObj.address.state))
     {
         error.push("State: "+reqObj.address.state+" is not a valid state. Please provide a valid String for State");
     }
